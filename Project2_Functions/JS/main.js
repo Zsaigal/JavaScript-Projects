78e048e7b2fbf5b27e36c4b406f957d82a0cc7f7
Zsaigal <139230936+Zsaigal@users.noreply.github.com>
2023-09-27 17:38:55 +0500

    Add files via upload

---
+++ Project2_Functions/JS/main.js
@@ -0,0 +1,12 @@
+function Function_with2_variables () {
+    var A="First_Variable";
+    document.getElementById("Button_Element").innerHTML=A;
+    var B="Second_Variable";
+    document.getElementById("Paragraph_Element").innerHTML=B;
+}
+
+function Concatenate_String() {
+    var X="I am learning";
+    X +=" a lot on this  course";
+    document.getElementById("Concatenate").innerHTML=X;
+}
