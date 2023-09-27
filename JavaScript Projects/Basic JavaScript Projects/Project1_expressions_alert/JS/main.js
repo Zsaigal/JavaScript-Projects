928392fe750f6264c851c30760deeca227108194
Zsaigal <139230936+Zsaigal@users.noreply.github.com>
2023-09-27 16:54:56 +0500

    Add files via upload

---
+++ JavaScript Projects/Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+
+
+document.write("way to display text in js");    
+
+var A="This is a string assigned to variable A";
+window.alert(A);
+
+document.write("<br>\"Ali\'s car broke down on the way\",said Joe.Using escape characters.")
+document.write("<br>This is an example of concatenating(connecting\\linking) a string using the plus sign operator."+"By writing both strings in quotation marks and putting a plus sign in between <br>")
+var B="Concatenated"+" string<br>"
+document.write(B)
+
+var D="Dad",C="Car",B="Tires";
+document.write(C)
+
+document.write(43+3)
+
+function My_First_Function() { // Defining a function
+    var str="This is the button text!"; // Defining a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML=str; // Putting the value of the variable into the HTML elementFromPoint with the "Button_Text" id 
+}
