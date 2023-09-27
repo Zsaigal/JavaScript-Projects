3cddbc7bd9c4d962b136e8592d527398a0c2c1ed
Zsaigal <139230936+Zsaigal@users.noreply.github.com>
2023-09-28 03:47:52 +0500

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,48 @@
+document.write(typeof "Word");
+
+document.write(" 23"+5);
+
+function infinity(){
+    document.getElementById("Too_large").innerHTML=3E310;
+}
+function neg_infinity(){
+    document.getElementById("Too_small").innerHTML=-3E310;
+}
+
+document.write(10>100);
+document.write(100>10);
+console.log(9+8);
+console.log(9<8);
+document.write(5==5);
+document.write(5==6);
+
+
+X=10;
+Y=10;
+document.write(X===Y);
+
+X=10;
+Y="Six";
+document.write(X===Y);
+
+X=10;
+Y="Ten";
+document.write(X===Y);
+
+X=10;
+Y=6;
+document.write(X===Y);
+
+document.write(5>4 && 6>1);
+
+document.write(5>5 && 6>1);
+
+document.write(5>6 || 6>7);
+
+document.write(5>5 || 6>1);
+
+document.write(!6>1);
+document.write(!6<1);
+
+
+
